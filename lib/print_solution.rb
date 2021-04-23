18e2fe0c1a44f2ea42491252dc4e237233d5e38b
AngelMoraDev <angelmomaa@gmail.com>
2021-04-23 12:11:48 -0500

    solving case 3

--- lib/print_solution.rb
+++ lib/print_solution.rb
@@ -9,16 +9,20 @@ class PrintSolution
   def self.merge_array(array_of_arrays)
     result = []
     array_of_arrays.each_with_index do |arr, index|
+      if index < array_of_arrays.length - 1
         next_array = array_of_arrays[index + 1]
         if arr[1] > next_array[0]
           new_array = arr + next_array
           new_array = new_array.minmax unless new_array.nil?
-          result << new_array sunless new_array.nil?
-        elsif arr[1] < next_array[0]
-          new_array = arr, next_array
           result << new_array unless new_array.nil?
+        elsif arr[1] < next_array[0]
+          # new_array = arr, next_array
+          # result << new_array unless new_array.nil?
+          result << arr unless arr.nil?
+          result << next_array
         end
+      end
     end
-    result[0]
+    result
   end
 end
