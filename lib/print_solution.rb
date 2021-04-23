5fe7109df2c0e2e8b23f996bbe8527769f4f4ba3
AngelMoraDev <angelmomaa@gmail.com>
2021-04-23 09:54:44 -0500

    fixing self.merge_array inner loop

--- lib/print_solution.rb
+++ lib/print_solution.rb
@@ -3,7 +3,7 @@
 # Performance / buenas prácticas
 # Rspec tests with dummy arrays
 require 'byebug'
-
+require 'benchmark'
 example_1 = [[1,5], [2,6]] #= [[1,6]
 example_2 = [[1,5], [6,8]] #= [[1,5], [6,8]]
 example_3 = [[1,3], [4,5], [6,7]] # = [[1,3], [4,5], [6,7]]
@@ -11,20 +11,29 @@ example_4 = [[1,5], [2,6], [8,9]] # = [[1,6], [8,9]]
 example_5 = [[1,5], [2,2], [2,3], [7,10]] # = [[1,5], [7,10]]
 
 class PrintSolution
+
+  #solution by repositioning
   def self.merge_array(array_of_arrays)
+    result = []
     first_arr = array_of_arrays[0]
     second_arr = array_of_arrays[1]
-    
+
     new_array = [first_arr[0], second_arr[1]]
     first_arr[1] > second_arr[0] ? new_array : array_of_arrays
     
     array_of_arrays.each do |left_array, right_array|
-      first_arr = left_array[0]# > second_arr[0]
+      byebug
+      first_arr = left_array[0]
       second_arr = right_array[1]
       new_array = [first_arr[0], second_arr[1]]
       new_array if first_arr[1] > second_arr[0]
+      result << new_array
     end
+    result
+    byebug
   end
+
+  # solution by sum and set minmax
 end
 
 p PrintSolution.merge_array(example_1)
