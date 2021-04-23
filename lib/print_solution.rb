a252b6cbc06355fe435a949a600dee43e9c1ca5f
AngelMoraDev <angelmomaa@gmail.com>
2021-04-23 10:51:37 -0500

    first test passing

--- lib/print_solution.rb
+++ lib/print_solution.rb
@@ -4,11 +4,6 @@
 # Rspec tests with dummy arrays
 require 'byebug'
 require 'benchmark'
-example_1 = [[1,5], [2,6]] #= [[1,6]]
-example_2 = [[1,5], [6,8]] #= [[1,5], [6,8]]
-example_3 = [[1,3], [4,5], [6,7]] # = [[1,3], [4,5], [6,7]]
-example_4 = [[1,5], [2,6], [8,9]] # = [[1,6], [8,9]]
-example_5 = [[1,5], [2,2], [2,3], [7,10]] # = [[1,5], [7,10]]
 
 class PrintSolution
 
@@ -26,12 +21,4 @@ class PrintSolution
     end
     result
   end
-
-  # solution by sum and set minmax
 end
-
-p PrintSolution.merge_array(example_1)
-p PrintSolution.merge_array(example_2)
-# p PrintSolution.merge_array(example_3)
-# p PrintSolution.merge_array(example_4)
-# p PrintSolution.merge_array(example_5)
