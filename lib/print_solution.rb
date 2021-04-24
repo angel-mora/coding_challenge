1e454ec9a7d21393e9b9a87b051d265b90933199
AngelMoraDev <angelmomaa@gmail.com>
2021-04-24 05:34:46 -0500

    refactoring considering edge cases

--- lib/print_solution.rb
+++ lib/print_solution.rb
@@ -24,21 +24,36 @@ class PrintSolution
     #   result
     # end
 
-    idx = 0
+    # idx = 0
     result = []
     arrays_length = arrays.length - 1
     arrays_length.times do
-      eval_merge = arrays[idx], arrays[idx+1]
-      if eval_merge[0][1] >= eval_merge[1][0] # change eval_merge[1][0] to loop over next elements
-        # eval_merge[each][0] something like this
+      # eval_merge = arrays[idx], arrays[idx+1]
+      # if eval_merge[0][1] >= eval_merge[1][0] # change eval_merge[1][0] to loop over next elements
+      #   # eval_merge[each][0] something like this
+
+      #   eval_merge = eval_merge.reduce(:+).minmax
+      #   result << eval_merge
+      # elsif eval_merge[0][1] < eval_merge[1][0]
+      #   result << eval_merge[0] unless result.include?(eval_merge[0])
+      #   result << eval_merge[1] unless result.include?(eval_merge[1])
+      # end
+      # idx += 1
+      arrays.each_with_index do |array, idx|
+        base = arrays[idx]
+        j = idx+1 unless idx+1 >= arrays_length
+        nxt = arrays[j]
+        if base[1] > nxt[0]
+          eval_merge = base, nxt
+          testing_method = eval_merge.reduce(:+).minmax
+          j += 1
+        elsif base[1] < nxt[0]
+          result << base unless result.include?(base)
+          result << nxt unless result.include?(nxt)
+          j += 1
+        end
         
-        eval_merge = eval_merge.reduce(:+).minmax
-        result << eval_merge
-      elsif eval_merge[0][1] < eval_merge[1][0]
-        result << eval_merge[0] unless result.include?(eval_merge[0])
-        result << eval_merge[1] unless result.include?(eval_merge[1])
       end
-      idx += 1
     end
     result
 
