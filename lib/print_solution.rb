3ae396c714d0aa605fda8d80b2c630cc1263c3d4
AngelMoraDev <angelmomaa@gmail.com>
2021-04-26 02:53:41 -0500

    refactor vars in aux methods

--- lib/print_solution.rb
+++ lib/print_solution.rb
@@ -7,18 +7,8 @@ require 'benchmark'
 
 class PrintSolution
 
-  def self.compare_base_with_remaining(base,nxt,tmp)
-    if base[1] >= nxt[0]
-      tmp[1..-1].each do |nxt|
-        break if base[1] < nxt[0] 
-        tmp -= [base, nxt]
-        base = [base, nxt].reduce(:+).minmax
-        tmp.unshift(base)
-      end
-    elsif base[1] < nxt[0]
-      tmp << base unless tmp.include?(base || merged)
-      tmp << nxt unless tmp.include?(nxt || merged)
-    end
+  def compare_base_with_remaining(base,nxt,tmp)
+    
   end
 
   def self.merge_array(arrays)
@@ -29,8 +19,18 @@ class PrintSolution
       arrays_length = tmp.length - 1
       break unless index < arrays_length
 
-      compare_base_with_remaining(base,nxt,tmp)
-      
+      #compare_base_with_remaining(base,nxt,tmp)
+      if base[1] >= nxt[0]
+        tmp[1..-1].each do |nxt|
+          break if base[1] < nxt[0] 
+          tmp -= [base, nxt]
+          base = [base, nxt].reduce(:+).minmax
+          tmp.unshift(base)
+        end
+      elsif base[1] < nxt[0]
+        tmp << base unless tmp.include?(base || merged)
+        tmp << nxt unless tmp.include?(nxt || merged)
+      end
     end
     tmp
   end
