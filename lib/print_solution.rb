486fa63bdf10460f94b416735b87f00cc8f1abbe
AngelMoraDev <angelmomaa@gmail.com>
2021-04-26 02:00:03 -0500

    fourth case completed

--- lib/print_solution.rb
+++ lib/print_solution.rb
@@ -16,10 +16,10 @@ class PrintSolution
       break unless index < arrays_length
 
       if base[1] >= nxt[0]
-        tmp = tmp - [base, nxt]
+        tmp -= [base, nxt]
         base = [base, nxt].reduce(:+).minmax
         tmp.unshift(base)
-        byebug
+        # byebug
         
         # # testing inside evaluation
         # if base[1] >= tmp[index][0]
@@ -34,31 +34,5 @@ class PrintSolution
       end
     end
     tmp
-
-    #result.unshift(final_merge.reduce(:+).minmax)
-    # arrays_length = arrays.length - 1
-    # idx = 0
-    # base = arrays[idx]
-    # arrays_length.times do
-    #   j = idx + 1
-    #   nxt = arrays[j]
-    #   condition = base[1] >= nxt[0]
-    #   until idx == arrays_length
-    #     case condition
-    #     when true
-    #       merged = [base, nxt].reduce(:+).minmax
-    #       base = merged
-    #       result << base
-    #     when false
-    #       result << base
-    #       result << nxt if nxt
-    #       base = nxt if nxt
-    #     end
-    #     idx += 1
-    #     j += 1
-    #   end
-    # end
-    # result
-
   end
 end
