a23fddc8040259d54d38adcdf6c25c2080acaedd
AngelMoraDev <angelmomaa@gmail.com>
2021-04-26 03:15:20 -0500

    fixing linters

--- lib/print_solution.rb
+++ lib/print_solution.rb
@@ -1,37 +1,34 @@
 # Tenemos un array con arrays adentro de length 2 en el cual se tienen que mergear los que se solapen
-
 # Performance / buenas practicas
 # Rspec tests with dummy arrays
+
 require 'byebug'
 require 'benchmark'
 
 class PrintSolution
-
-  def compare_base_with_remaining(base,nxt,tmp)
-    
-  end
+  def compare_base_with_remaining(base, nxt, arrays); end
 
   def self.merge_array(arrays)
-    tmp = arrays
-    tmp.each_with_index do |_, index|
-      base = tmp[index]
-      nxt = tmp[index + 1]
-      arrays_length = tmp.length - 1
+    arrays.each_with_index do |_, index|
+      base = arrays[index]
+      nxt = arrays[index + 1]
+      arrays_length = arrays.length - 1
       break unless index < arrays_length
 
-      #compare_base_with_remaining(base,nxt,tmp)
+      # compare_base_with_remaining(base,nxt,arrays)
       if base[1] >= nxt[0]
-        tmp[1..-1].each do |nxt|
-          break if base[1] < nxt[0] 
-          tmp -= [base, nxt]
+        arrays[1..-1].each do |nxt|
+          break if base[1] < nxt[0]
+
+          arrays -= [base, nxt]
           base = [base, nxt].reduce(:+).minmax
-          tmp.unshift(base)
+          arrays.unshift(base)
         end
       elsif base[1] < nxt[0]
-        tmp << base unless tmp.include?(base || merged)
-        tmp << nxt unless tmp.include?(nxt || merged)
+        arrays << base unless arrays.include?(base)
+        arrays << nxt unless arrays.include?(nxt)
       end
     end
-    tmp
+    arrays
   end
 end
